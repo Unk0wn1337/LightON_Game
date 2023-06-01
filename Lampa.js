e643adcfe1e90f15b94713faa69b96b77f20a7de
Unk0wn1337 <kelogames@gmail.com>
2023-06-01 15:06:04 +0200

    baloldalt nem jó, kattitnast megkell normálisan csinálmi

--- Lampa.js
+++ Lampa.js
@@ -9,17 +9,25 @@ class Lampa{
         this.#divElem.addClass("kor");
         this.#divElem.addClass(allapot === 0 ? "sarga" : "zold");
         szuloELEM.append(this.#divElem);
-    
+        this.#divElem.on("click",() => {
+            this.#kattintasTrigger();
+        });
+
         
     }
     setAllapot(){
-
+     
     }
     #szinBeallit(){
+        
 
     }
+
     #kattintasTrigger(){
         const esemenyTrigger = new CustomEvent("kapcsolas", {detail: this});
+        this.#allapot = this.#allapot === 0 ? 1 : 0;
+        this.#divElem.removeClass(this.#allapot === 0 ? "zold" : "sarga");
+        this.#divElem.addClass(this.#allapot === 0 ? "sarga" : "zold");
         window.dispatchEvent(esemenyTrigger);
     }
 
